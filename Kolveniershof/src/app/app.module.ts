ba7c77acda7ae1391bc282dc4cd688e544a395ec
reeven govaert <govaertreeven@gmail.com>
2019-09-30 21:34:56 +0200

    added nav component

--- Kolveniershof/src/app/app.module.ts
+++ Kolveniershof/src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-
+import { MatMenuModule } from "@angular/material/menu";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { LoginComponent } from "./components/login/login.component";
 import { PageNotFoundComponent } from "./components/page-not-found/page-not-found.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatIconModule } from "@angular/material/icon";
+import { NavComponent } from "./components/nav/nav.component";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatFormFieldModule } from "@angular/material/form-field";
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, PageNotFoundComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    PageNotFoundComponent,
+    NavComponent
+  ],
+  imports: [
+    MatMenuModule,
+    MatIconModule,
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatFormFieldModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
