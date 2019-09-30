aea0799b1d56a3f60332b82f1146941a61ed4f60
reeven govaert <govaertreeven@gmail.com>
2019-09-30 20:21:24 +0200

    routing deels toegevoegd voor login

--- Kolveniershof/src/app/modules/routing/routing.module.ts
+++ Kolveniershof/src/app/modules/routing/routing.module.ts
@@ -1,12 +1,18 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { LoginComponent } from "../../components/login/login.component";
+import { RouterModule, Routes } from "@angular/router";
 
-
+const appRoutes: Routes = [
+  { path: "login", component: LoginComponent },
+  { path: "home", component: LoginComponent },
+  { path: "", redirectTo: "login", pathMatch: "full" },
+  { path: "**", component: LoginComponent }
+];
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(appRoutes), CommonModule],
+  exports: [RouterModule]
 })
-export class RoutingModule { }
+export class RoutingModule {}
