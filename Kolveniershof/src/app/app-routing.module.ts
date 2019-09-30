f4b003882d5886c9545b57fd59b319c4d444f117
reeven govaert <govaertreeven@gmail.com>
2019-09-30 20:53:44 +0200

    added routing the right way, changed in angular 8

--- Kolveniershof/src/app/app-routing.module.ts
+++ Kolveniershof/src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { LoginComponent } from "src/app/components/login/login.component";
 
+import { RouterModule, Routes } from "@angular/router";
+import { PageNotFoundComponent } from "src/app/components/page-not-found/page-not-found.component";
 
-const routes: Routes = [];
+const appRoutes: Routes = [
+  { path: "login", component: LoginComponent },
+  { path: "home", component: LoginComponent },
+  { path: "", redirectTo: "login", pathMatch: "full" },
+  { path: "**", component: PageNotFoundComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  declarations: [],
+  imports: [RouterModule.forRoot(appRoutes), CommonModule],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- Kolveniershof/src/app/app.module.ts
+++ Kolveniershof/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { LoginComponent } from "./components/login/login.component";
-import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { PageNotFoundComponent } from "./components/page-not-found/page-not-found.component";
 
 @NgModule({
   declarations: [AppComponent, LoginComponent, PageNotFoundComponent],
