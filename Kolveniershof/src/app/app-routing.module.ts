607acd878db1c420846a901571e8cfe036d63e85
reeveng <reevengovaert@gmail.com>
2019-10-11 19:23:42 +0200

    change link to api

--- Kolveniershof/src/app/app-routing.module.ts
+++ Kolveniershof/src/app/app-routing.module.ts
@@ -1,17 +1,17 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { LoginComponent } from "src/app/user/login/login.component";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from 'src/app/user/login/login.component';
 
-import { RouterModule, Routes } from "@angular/router";
-import { PageNotFoundComponent } from "src/app/page-not-found/page-not-found.component";
-import { WeekScheduleComponent } from "./week-schedule/week-schedule.component";
+import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from 'src/app/page-not-found/page-not-found.component';
+import { WeekScheduleComponent } from './week-schedule/week-schedule.component';
 
 const appRoutes: Routes = [
-  { path: "login", component: LoginComponent },
-  { path: "home", component: LoginComponent },
-  { path: "week", component: WeekScheduleComponent },
-  { path: "", redirectTo: "login", pathMatch: "full" },
-  { path: "**", component: PageNotFoundComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: LoginComponent },
+  { path: 'week', component: WeekScheduleComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
--- Kolveniershof/src/environments/environment.ts
+++ Kolveniershof/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: "https://kolv02-backend.herokuapp.com/API"
+  apiUrl: "https://kolv02-backend.herokuapp.com"
 };
 
 /*
