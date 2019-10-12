7938378a94845220fc1c8644a76393b0fb386802
Wout Maes <wout.maes@student.hogent.be>
2019-10-12 19:12:37 +0200

    Layout of DatePicker

--- Kolveniershof/src/app/picto-agenda/picto-agenda.component.ts
+++ Kolveniershof/src/app/picto-agenda/picto-agenda.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { faGreaterThan } from "@fortawesome/free-solid-svg-icons";
+import { faLessThan } from "@fortawesome/free-solid-svg-icons";
 
 @Component({
-  selector: 'app-picto-agenda',
-  templateUrl: './picto-agenda.component.html',
-  styleUrls: ['./picto-agenda.component.css']
+  selector: "app-picto-agenda",
+  templateUrl: "./picto-agenda.component.html",
+  styleUrls: ["./picto-agenda.component.css"]
 })
 export class PictoAgendaComponent implements OnInit {
+  faGreaterThan = faGreaterThan;
+  faLessThan = faLessThan;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
