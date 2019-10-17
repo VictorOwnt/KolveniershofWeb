f835ef3963d897a673073df53379cd2b4cb00e6f
Sebastien <sebastien.wojtyla.y3274@student.hogent.be>
2019-10-17 17:46:41 +0200

    Retrieve workdays and display info(Not finished yet)

--- Kolveniershof/src/app/week-schedule/algemeen-week-schedule/algemeen-week-schedule.component.ts
+++ Kolveniershof/src/app/week-schedule/algemeen-week-schedule/algemeen-week-schedule.component.ts
@@ -5,6 +5,7 @@ import { WorkDayDataService } from "src/app/workDay.data.service";
 import { Workday } from "src/app/domain/workday.model";
 import { Observable } from "rxjs";
 import { HttpClient } from "@angular/common/http";
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: "app-algemeen-week-schedule",
@@ -14,32 +15,8 @@ import { HttpClient } from "@angular/common/http";
 export class AlgemeenWeekScheduleComponent implements OnInit {
   faPencilAlt = faPencilAlt;
   faTrash = faTrash;
-  public loadingError$ = this._workDayDataService.loadingError$;
-
-  private _date = new Date(2019, 1, 1);
-  private _fetchWorkDays$: Observable<
-    Workday
-  > = this._workDayDataService.getWorkDayByDate(this._date);
-
-  /* @Input() public workday: Workday;*/
-
-  constructor(
-    private _workDayDataService: WorkDayDataService,
-    private http: HttpClient
-  ) {}
-
-  /*  getWorkDayByDate(_date): Observable<Workday> {
-    return this._fetchWorkDays$;
-  }*/
-
-  get workday$(): Observable<Workday> {
-    console.log(this._fetchWorkDays$);
-    return this._fetchWorkDays$;
-  }
-
-  addNewWorkDay(workDay) {
-    this._workDayDataService.addNewWorkDay(workDay).subscribe;
-  }
+  @Input() public workday: Workday;
+  constructor() {}  
 
   ngOnInit() {}
 }
--- Kolveniershof/src/app/week-schedule/week-schedule.component.ts
+++ Kolveniershof/src/app/week-schedule/week-schedule.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from "@angular/core";
 import { faGreaterThan } from "@fortawesome/free-solid-svg-icons";
 import { faLessThan } from "@fortawesome/free-solid-svg-icons";
+import { Observable } from 'rxjs';
+import { Workday } from '../domain/workday.model';
+import { WorkDayDataService } from '../workDay.data.service';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: "app-week-schedule",
@@ -10,8 +15,22 @@ import { faLessThan } from "@fortawesome/free-solid-svg-icons";
 export class WeekScheduleComponent implements OnInit {
   faGreaterThan = faGreaterThan;
   faLessThan = faLessThan;
-
-  constructor() {}
+  private date = new Date(2019,1,1);
+  public loadingError$ = this._workDayDataService.loadingError$;
+  private _fetchWorkday$: Observable<Workday[]> = this._workDayDataService.workdays$; //Haalt momenteel alles op, was makkelijker om zo te testen
+ 
+  constructor(
+    private _workDayDataService: WorkDayDataService,
+    private http: HttpClient
+  ) {}
 
   ngOnInit() {}
+
+  get workdays$(): Observable<Workday[]>{
+   
+    return this._fetchWorkday$;
+
+  }
+
+  
 }
