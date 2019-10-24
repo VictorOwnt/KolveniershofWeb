fbb9e51b5e0b000fe011ed0c736aea2e313302e4
Wout Maes <wout.maes@student.hogent.be>
2019-10-24 09:24:08 +0200

    Filled WeekSchedule with Backend Data

--- Kolveniershof/src/app/week-schedule/algemeen-week-schedule/algemeen-week-schedule.component.ts
+++ Kolveniershof/src/app/week-schedule/algemeen-week-schedule/algemeen-week-schedule.component.ts
@@ -1,11 +1,7 @@
 import { Component, OnInit, Input } from "@angular/core";
 import { faPencilAlt } from "@fortawesome/free-solid-svg-icons";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
-import { WorkDayDataService } from "src/app/workDay.data.service";
 import { Workday } from "src/app/domain/workday.model";
-import { Observable } from "rxjs";
-import { HttpClient } from "@angular/common/http";
-import { map } from 'rxjs/operators';
 
 @Component({
   selector: "app-algemeen-week-schedule",
@@ -16,7 +12,7 @@ export class AlgemeenWeekScheduleComponent implements OnInit {
   faPencilAlt = faPencilAlt;
   faTrash = faTrash;
   @Input() public workday: Workday;
-  constructor() {}  
+  constructor() {}
 
   ngOnInit() {}
 }
