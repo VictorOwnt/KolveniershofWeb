40e0a256722571fe9a7470f798737d13284341d5
Wout Maes <wout.maes@student.hogent.be>
2019-10-29 10:10:48 +0100

    Added Busschema component

---
+++ src/app/busschema/busschema.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-busschema',
+  templateUrl: './busschema.component.html',
+  styleUrls: ['./busschema.component.css']
+})
+export class BusschemaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
