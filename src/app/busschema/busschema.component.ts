b4bf4328eb6746d355767dedd9374cf5c6affe96
Wout Maes <wout.maes@student.hogent.be>
2019-10-31 09:51:00 +0100

    Get data from backend into busschema works partially

--- src/app/busschema/busschema.component.ts
+++ src/app/busschema/busschema.component.ts
@@ -3,6 +3,7 @@ import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { MatDatepickerInputEvent } from "@angular/material";
 import { BusUnitDataService } from "../busUnit.data.service";
+import { BusUnit } from "../domain/busUnit.model";
 
 @Component({
   selector: "app-busschema",
@@ -10,5 +11,30 @@ import { BusUnitDataService } from "../busUnit.data.service";
   styleUrls: ["./busschema.component.css"]
 })
 export class BusschemaComponent implements OnInit {
-  ngOnInit() {}
+  public loadingError$ = this._busUnitDataService.loadingError$;
+  private _fetchBusses$: Observable<BusUnit[]> = this._busUnitDataService
+    .busUnits$;
+  //private _busUnit: BusUnit;
+
+  constructor(private _busUnitDataService: BusUnitDataService) {
+    //this._fetchBusses$.subscribe(value => (this._busUnit = value));
+  }
+
+  ngOnInit() {
+    // this.workday$.subscribe(e => console.log(e));
+  }
+
+  get busUnit$(): Observable<BusUnit[]> {
+    return this._fetchBusses$;
+  }
+
+  // get busUnit$(): BusUnit {
+  //   return this._busUnit;
+  // }
+
+  // set workday(date: Date) {
+  // this._busUnitDataService
+  // .busUnits$
+  // .subscribe(val => (this._busUnit = val));
+  //}
 }
