d634747e1d9191f98fca5fa691b821b3740ba8e4
Wout Maes <wout.maes@student.hogent.be>
2019-10-31 16:32:04 +0100

    Wrote cypress test for login

---
+++ cypress/integration/authentication.spec.js
@@ -0,0 +1,16 @@
+describe('Login Page', () => {
+    beforeEach(() => {});
+
+    it('logintest', () => {
+        cy.login();
+    });
+
+    it('login page', () => {
+        cy.visit('http://localhost:4200/login');
+        cy.get('[data-cy=login-email]').type('test10@gmail.com');
+        cy.get('[data-cy=login-password]').type('test00##');
+        cy.get('[data-cy=login-button').click();
+        // login name should be in the title balk
+        cy.contains('test10@gmail.com');
+    });
+});
