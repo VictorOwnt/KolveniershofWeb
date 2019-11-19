f6c1bad684edba443b24b7e5416e3347fbfa23e8
Victor Van hulle <victor.vanhulle.w3296@student.hogent.be>
2019-11-19 22:39:24 +0100

    Bugfix changes
    NOT WORKING YET

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -14,7 +14,7 @@ import { map } from 'rxjs/operators';
 import * as zxcvbn from 'zxcvbn';
 import * as $ from 'jquery';
 
-function comparePasswords(control: AbstractControl): { [key: string]: any } {
+function comparePasswords(control: FormGroup): { [key: string]: any } {
   const password = control.get('password');
   const confirmPassword = control.get('confirmPassword');
   return password.value === confirmPassword.value
@@ -35,9 +35,9 @@ function serverSideValidateEmail(authService: AuthenticationService): ValidatorF
   };
 }
 
-function passwordStrength(control: AbstractControl) { // TODO
-  if (zxcvbn(control.value).score < 2) {
-    return { passwordStrenght: true };
+function passwordStrength(control: AbstractControl): { [key: string]: any }  { // TODO
+  if (zxcvbn(String(control.value)).score < 2) {
+    return { passwordStrenghtValue: true };
   } else {
     return null;
   }
@@ -54,6 +54,7 @@ export class RegisterComponent implements OnInit {
   public startDate = new Date();
   hidePassword = true;
   hideConfirmPassword = true;
+  public url: string;
 
   constructor(
     private authService: AuthenticationService,
@@ -75,12 +76,13 @@ export class RegisterComponent implements OnInit {
           password: ['', [Validators.required, passwordStrength]],
           confirmPassword: ['', Validators.required]
         },
-        { validators: [Validators.required, comparePasswords] }
+        { validator: comparePasswords }
       ),
       street: [''],
       city: [''],
       postalCode: [''],
-      birthday: ['', Validators.required]
+      birthday: ['', Validators.required],
+      picture: ['']
     });
   }
 
@@ -97,12 +99,12 @@ export class RegisterComponent implements OnInit {
   }
 
   getPasswordErrorMessage() {
-    return this.user.controls.passwordGroup.hasError('required')
+    return this.user.controls.passwordGroup.hasError('required', 'password')
       ? 'Wachtwoord is verplicht' :
-      this.user.controls.passwordGroup.get('password').hasError('passwordStrength')
-        ? 'Wachtwoord is niet sterk genoeg.' :
-          this.user.controls.passwordGroup.hasError('passwordsDiffer')
-            ? 'Wachtwoorden komen niet overeen.' : '';
+        this.user.controls.passwordGroup.hasError('passwordStrenghtValue', 'password')
+          ? 'Wachtwoord is niet sterk genoeg.' :
+            this.user.controls.passwordGroup.hasError('passwordsDiffer') // TODO - Doesn't work properly
+              ? 'Wachtwoorden komen niet overeen.' : '';
   }
 
   getBirthdayErrorMessage() {
@@ -116,7 +118,7 @@ export class RegisterComponent implements OnInit {
         this.user.value.passwordGroup.password,
         this.user.value.firstName,
         this.user.value.lastName,
-        // this.user.value.picture, // TODO - Picture
+        //this.user.value.picture, // TODO - Picture
         this.user.value.birthday, // TODO - Correct date
         this.user.value.street,
         this.user.value.city,
