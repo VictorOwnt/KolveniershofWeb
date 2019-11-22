8ee9c29e62ef23c674aaa7edf4f71439dfda7460
Jakob Lierman <jakob.lierman@gmail.com>
2019-11-22 20:05:38 +0100

    Dynamic schedule-item design

--- src/app/admin/schedule/schedule-item/schedule-item.component.ts
+++ src/app/admin/schedule/schedule-item/schedule-item.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {ActivityUnit} from '../../../shared/models/activityUnit.model';
+import {LunchUnit} from '../../../shared/models/lunchUnit.model';
+import {User} from '../../../shared/models/user.model';
 
 @Component({
   selector: 'app-schedule-item',
@@ -6,10 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./schedule-item.component.scss']
 })
 export class ScheduleItemComponent implements OnInit {
+  @Input() private item: any;
+  private title: string;
+  private mentors: User[];
+  private clients: User[];
 
   constructor() { }
 
   ngOnInit() {
+    if (this.item instanceof ActivityUnit) {
+      this.title = this.item.activity.name;
+      this.mentors = this.item.mentors;
+      this.clients = this.item.clients;
+    } else if (this.item instanceof LunchUnit) {
+      this.title = this.item.lunch;
+      this.mentors = this.item.mentors;
+      this.clients = this.item.clients;
+    }
+  }
+
+  edit() {
+    // TODO - edit
+  }
+
+  delete() {
+    // TODO - delete
   }
 
 }
