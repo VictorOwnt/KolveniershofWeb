1b1afb3bbc729613775a36383f3ec8cb519df661
Jakob Lierman <jakob.lierman@gmail.com>
2019-11-25 15:23:45 +0100

    Added all activityUnit routes

--- src/app/services/activity.data.service.ts
+++ src/app/services/activity.data.service.ts
@@ -14,10 +14,6 @@ export class ActivityDataService {
 
     constructor(private http: HttpClient) {}
 
-    updateActivityUnit(activityUnit: ActivityUnit) {
-        return this.http.patch(`${API_URL}/units/${activityUnit.id}`, activityUnit.toJSON());
-    }
-
     get activities(): Observable<Activity[]> {
         return this.http
           .get(`${API_URL}/activities`)
@@ -25,6 +21,12 @@ export class ActivityDataService {
                 this.loadingError$.next(error.statusText);
                 return of(null);
           }), map((list: any[]): Activity[] => list.map(Activity.fromJSON)));
+    }
+
+    getActivityById(id: string): Observable<Activity> {
+      return this.http
+        .get(`${API_URL}/activities/id/${id}`)
+        .pipe(map(Activity.fromJSON));
     }
 
     postActivity(activity: Activity): Observable<Activity> {
@@ -40,8 +42,37 @@ export class ActivityDataService {
     }
 
     deleteActivity(id: string): void {
-        this.http
-          .delete(`${API_URL}/activities/id/${id}`)
-          .pipe(map(Activity.fromJSON));
+        this.http.delete(`${API_URL}/activities/id/${id}`);
+    }
+
+    get activityUnits(): Observable<ActivityUnit[]> {
+      return this.http
+        .get(`${API_URL}/activities/units`)
+        .pipe(catchError(error => {
+          this.loadingError$.next(error.statusText);
+          return of(null);
+        }), map((list: any[]): ActivityUnit[] => list.map(ActivityUnit.fromJSON)));
+    }
+
+    getActivityUnitById(id: string): Observable<ActivityUnit> {
+      return this.http
+        .get(`${API_URL}/activities/units/id/${id}`)
+        .pipe(map(ActivityUnit.fromJSON));
+    }
+
+    postActivityUnit(activityUnit: ActivityUnit): Observable<ActivityUnit> {
+      return this.http
+        .post(`${API_URL}/activities/units`, activityUnit)
+        .pipe(map(ActivityUnit.fromJSON));
+    }
+
+    patchActivityUnit(activityUnit: ActivityUnit): Observable<ActivityUnit> {
+      return this.http
+        .patch(`${API_URL}/activities/units/id/${activityUnit.id}`, activityUnit)
+        .pipe(map(ActivityUnit.fromJSON));
+    }
+
+    deleteActivityUnit(id: string): void {
+      this.http.delete(`${API_URL}/activities/units/id/${id}`);
     }
 }
