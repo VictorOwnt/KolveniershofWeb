1acd2f87d6cb8d5ec90f9ce5f0fde07b9d40e486
Jakob Lierman <jakob.lierman@gmail.com>
2019-11-27 17:31:53 +0100

    Added comment-list mailto functionality

--- src/app/admin/schedule/comment-list/comment-list.component.ts
+++ src/app/admin/schedule/comment-list/comment-list.component.ts
@@ -12,7 +12,24 @@ export class CommentListComponent implements OnInit {
 
   constructor(private auth: AuthenticationService) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  mailto(comment: Comment): string {
+    const mailContent = [];
+    mailContent.push(
+      'Beste ' + comment.client.firstName + ',',
+      'Uw opmerking werd beantwoord.',
+      'Uw opmerking:\n' + comment.comment,
+      'Antwoord:\n' + 'HIER ANTWOORDEN',
+      'Met vriendelijke groeten\n' + this.auth.currentUser.fullName + '\nHet Kolveniershof'
+    );
+
+    const recepient = encodeURI(comment.client.email);
+    const copy = 'cc=' + encodeURI(''); // TODO - Cc
+    const blindCopy = 'bcc=' + encodeURI(''); // TODO - Bcc
+    const subject = 'subject=' + encodeURI('Kolveniershof: Antwoord op uw opmerking');
+    const body = 'body=' + encodeURI(mailContent.join('\n\n'));
+    return 'mailto:' + recepient + '?' + copy + '&' + blindCopy + '&' + subject + '&' + body;
   }
 
 }
