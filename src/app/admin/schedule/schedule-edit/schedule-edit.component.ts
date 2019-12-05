c7089eb84ed4c4cd8ac8a80a3d89df033d8959b6
Jakob Lierman <jakob.lierman@gmail.com>
2019-12-05 01:48:02 +0100

    Select activity functionality
    WIP

--- src/app/admin/schedule/schedule-edit/schedule-edit.component.ts
+++ src/app/admin/schedule/schedule-edit/schedule-edit.component.ts
@@ -1,14 +1,15 @@
-import {Component, Inject, OnInit, ViewEncapsulation} from '@angular/core';
-import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
+import {Component, Inject, OnInit} from '@angular/core';
+import {MAT_DIALOG_DATA} from '@angular/material/dialog';
 import {AuthenticationService} from '../../../user/authentication.service';
 import {ActivityUnit} from '../../../shared/models/activityUnit.model';
-import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {FirebaseService} from '../../../services/firebase.service';
-import {DomSanitizer} from '@angular/platform-browser';
 import {Observable} from 'rxjs';
 import {ActivityDataService} from '../../../services/activity.data.service';
 import {Activity} from '../../../shared/models/activity.model';
 import {map, startWith} from 'rxjs/operators';
+import {User} from '../../../shared/models/user.model';
+import {UserDataService} from '../../../services/user.data.service';
 
 @Component({
   selector: 'app-schedule-edit',
@@ -16,53 +17,68 @@ import {map, startWith} from 'rxjs/operators';
   styleUrls: ['./schedule-edit.component.scss'],
 })
 export class ScheduleEditComponent implements OnInit {
-  activities: Activity[];
-  myControl = new FormControl();
-  options: string[] = [];
-  filteredOptions: Observable<string[]>;
-  public activityUnitForm: FormGroup;
-  public errorMsg = '';
-  imageUrl: any = null;
+  unit: any = null;
+  activities: Activity[] = [];
+  activityImgUrl: any = null;
+  mentors: User[] = [];
+  clients: User[] = [];
+  isActivity = false;
+  filteredActivities: Observable<Activity[]>;
+  public unitFormGroup: FormGroup;
 
   constructor(
-      @Inject(MAT_DIALOG_DATA) public activityUnit: ActivityUnit,
-      public dialogRef: MatDialogRef<ScheduleEditComponent>,
-      private fb: FormBuilder,
-      private auth: AuthenticationService,
-      private firebaseService: FirebaseService,
-      private sanitizer: DomSanitizer,
-      private activityDataService: ActivityDataService) {
-
+    @Inject(MAT_DIALOG_DATA) data: any,
+    private auth: AuthenticationService,
+    private fb: FormBuilder,
+    private firebaseService: FirebaseService,
+    private activityDataService: ActivityDataService,
+    /*private lunchDataService: LunchDataService,*/
+    private userDataService: UserDataService
+  ) {
+    this.unit = data;
+    this.isActivity = data instanceof ActivityUnit;
+    activityDataService.activities$.subscribe(activities => this.activities = activities);
+    userDataService.mentors$.subscribe(mentors => this.mentors = mentors);
+    userDataService.clients$.subscribe(clients => this.clients = clients);
   }
+
   ngOnInit() {
-    if (this.activityUnit) {
-      this.imageUrl = '';
-      this.firebaseService.lookupFileDownloadUrl(this.activityUnit.activity.icon, 'icon').subscribe(img => this.imageUrl = img);
+    if (this.isActivity) {
+      if (this.unit) {
+        // Download activity icon
+        this.activityImgUrl = '';
+        this.firebaseService.lookupFileDownloadUrl(this.unit.activity.icon, 'icon').subscribe(img => this.activityImgUrl = img);
+      }
+      // FormGroup for activityUnit
+      this.unitFormGroup = this.fb.group({
+        activity: [this.unit ? this.unit.activity : null, Validators.required],
+        mentors: [this.unit ? this.unit.mentors : null, Validators.required],
+        clients: [this.unit ? this.unit.clients : null, Validators.required]
+      });
+      // Filter activities
+      this.filteredActivities = this.unitFormGroup.controls.activity.valueChanges
+        .pipe(startWith(''), map(value => this._filterActivities(value)));
+    } else {
+      // FormGroup for lunchUnit
+      this.unitFormGroup = this.fb.group({
+        lunch: [this.unit ? this.unit.lunch : null, Validators.required],
+        mentors: [this.unit ? this.unit.mentors : null, Validators.required],
+        clients: [this.unit ? this.unit.clients : null, Validators.required]
+      });
     }
-
-    this.activityUnitForm = this.fb.group({
-      name: [this.activityUnit ? this.activityUnit.activity.name : '', Validators.required],
-    });
-
-    this.activityDataService.activities$.subscribe( a => a.forEach(act => this.options.push(act.name)));
-    this.activityDataService.activities$.subscribe(ac => this.activities = ac);
-
-    this.filteredOptions = this.myControl.valueChanges
-        .pipe(
-            startWith(''),
-            map(value => this._filter(value))
-        );
   }
 
-  private _filter(value: string): string[] {
-    const filterValue = value.toLowerCase();
-
-    return this.options.filter(option => option.toLowerCase().includes(filterValue));
+  private _filterActivities(value: string): Activity[] {
+    const filterValue = value.toLowerCase().trim();
+    return this.activities.filter(option => option.name.toLowerCase().includes(filterValue));
   }
 
-  preview(fileInput: any) {
-    const activity = this.activities.filter( a => a.name === fileInput.valueOf() );
-    this.firebaseService.lookupFileDownloadUrl(activity[0].icon, 'icon').subscribe(x => this.imageUrl = x);
+  compareUsers(user1: User, user2: User) {
+    return user1 && user2 ? user1.id === user2.id : user1 === user2;
+  }
+
+  previewActivity(activity: Activity) {
+    this.firebaseService.lookupFileDownloadUrl(activity.icon, 'icon').subscribe(x => this.activityImgUrl = x);
   }
 
   submitUnit() {
