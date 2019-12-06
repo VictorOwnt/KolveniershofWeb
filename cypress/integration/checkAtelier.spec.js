cdbf68304a6f220f248309682dc7e3a539f946e0
Wout Maes <wout.maes@student.hogent.be>
2019-12-06 14:28:41 +0100

    Wrote extra cypress test with Reeven Govaert

--- cypress/integration/checkAtelier.spec.js
+++ cypress/integration/checkAtelier.spec.js
@@ -1,27 +1,24 @@
 describe('Check ateliers', function () {
-    it('Check the length of the atelier', function () {
-        cy.visit('http://localhost:4200/a');
+    beforeEach(function () {
+        cy.login();
     });
+    // it('Check the length of the atelier', function () {
+    //     cy.visit('http://localhost:4200/a');
+    // });
 
     it('mock activities get', function () {
         cy.server();
         cy.route({
+            delay: 2000,
             method: 'GET',
-            url: 'https://kolv02-backend.herokuapp.com/API/activities',
-            status: 204,
-            response: [{
-                    "_id": "5dce5258fb69c00affb083f4",
-                    "name": "Bakken",
-                    "icon": "icons/icon-mixer.svg"
-                },
-                {
-                    "_id": "5dce5258fb69c00affb083f5",
-                    "name": "Balanske",
-                    "icon": "icons/icon-sofa.svg"
-                }
-            ]
+            url: '/activities',
+            status: 200,
+            response: 'fixture:activities.json'
         });
 
+        it('Check the length of the atelier', function () {
+            cy.visit('http://localhost:4200/a');
+        });
         cy.get('[data-cy=Activities]').should('have.length', 2);
     });
 
--- cypress/integration/registration.spec.js
+++ cypress/integration/registration.spec.js
@@ -6,7 +6,7 @@ describe('Fill in registration form', function () {
     it('Fill in form correct => button enabled', function () {
         cy.get('[data-cy=firstNameInput]').type('Wout')
             .get('[data-cy=lastNameInput]').type('Maes')
-            .get('[data-cy=emailInput]').type('maeswout1@gmail.com')
+            .get('[data-cy=emailInput]').type('maeswout11@gmail.com')
             .get('[data-cy=passwordInput]').type('test00##')
             .get('[data-cy=repeatPasswordInput]').type('test00##')
             .get('[data-cy=birthdateInput]').type('01/01/1999')
