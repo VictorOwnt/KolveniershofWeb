24bcef6774cc7dadeb53b165e9f867691a7bf34c
Jakob Lierman <jakob.lierman@gmail.com>
2019-12-10 16:25:24 +0100

    Bugfix return home button

--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -6,6 +6,7 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {MatButtonModule, MatDatepickerModule, MatFormFieldModule, MatIconModule, MatInputModule} from '@angular/material';
 import {FlexModule} from '@angular/flex-layout';
 import {ForbiddenComponent} from './forbidden/forbidden.component';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import {ForbiddenComponent} from './forbidden/forbidden.component';
     FlexModule,
     MatInputModule,
     MatButtonModule,
-    MatDatepickerModule
+    MatDatepickerModule,
+    RouterModule
   ],
   exports: [
     LoginComponent,
