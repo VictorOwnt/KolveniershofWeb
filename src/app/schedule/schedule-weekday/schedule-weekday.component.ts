9c101fa00dad58ab1a30e228ec9c0c41c28a67aa
Jakob Lierman <jakob.lierman@gmail.com>
2019-12-12 12:23:09 +0100

    Made schedule weekday compatible with template

--- src/app/schedule/schedule-weekday/schedule-weekday.component.ts
+++ src/app/schedule/schedule-weekday/schedule-weekday.component.ts
@@ -1,8 +1,10 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Workday} from '../../models/workday.model';
 import {WorkdayDataService} from '../../services/workday.data.service';
-import { FirebaseService } from 'src/app/services/firebase.service';
-import { Observable } from 'rxjs';
+import {FirebaseService} from 'src/app/services/firebase.service';
+import {Observable} from 'rxjs';
+import {WorkdayTemplate} from '../../models/workdayTemplate.model';
+import {WorkdayTemplateDataService} from '../../services/workdayTemplate.data.service';
 
 @Component({
   selector: 'app-schedule-weekday',
@@ -10,17 +12,28 @@ import { Observable } from 'rxjs';
   styleUrls: ['./schedule-weekday.component.scss']
 })
 export class ScheduleWeekdayComponent implements OnInit {
-  @Input() workday: Workday;
+  @Input() workday: Workday | WorkdayTemplate;
   @Input() isAdmin: boolean;
+  isTemplate: boolean;
   icon: Observable<string | null>;
 
   constructor(
     private workdayDataService: WorkdayDataService,
+    private workdayTemplateDataService: WorkdayTemplateDataService,
     private firebaseService: FirebaseService
-  ) {}
+  ) {
+  }
 
   ngOnInit() {
-    this.icon = this.firebaseService.lookupFileDownloadUrl(this.workdayDataService.getDayIcon(this.workday.date.getDay()), 'icon');
+    if (this.workday instanceof Workday) {
+      this.isTemplate = false;
+      this.icon = this.firebaseService
+        .lookupFileDownloadUrl(this.workdayDataService.getDayIcon(this.workday.date.getDay()), 'icon');
+    } else if (this.workday instanceof WorkdayTemplate) {
+      this.isTemplate = true;
+      this.icon = this.firebaseService
+        .lookupFileDownloadUrl(this.workdayTemplateDataService.getDayIcon(this.workday.dayNumber), 'icon');
+    }
   }
 
   newUnit(type: string, isAm: boolean = null) {
@@ -29,7 +42,11 @@ export class ScheduleWeekdayComponent implements OnInit {
 
   changeHoliday(holiday: boolean) {
     this.workday.holiday = holiday;
-    this.workdayDataService.patchWorkday(this.workday);
+    if (this.isTemplate) {
+      this.workdayTemplateDataService.patchWorkdayTemplate(this.workday as WorkdayTemplate);
+    } else {
+      this.workdayDataService.patchWorkday(this.workday as Workday);
+    }
   }
 
   viewComments() {
