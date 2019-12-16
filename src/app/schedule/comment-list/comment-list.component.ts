e2308f2c98855cec8e3d1ed586f533b7e1e78839
Victor Van hulle <victor.vanhulle.w3296@student.hogent.be>
2019-12-16 17:22:25 +0100

    Loading profile picture comment

--- src/app/schedule/comment-list/comment-list.component.ts
+++ src/app/schedule/comment-list/comment-list.component.ts
@@ -1,6 +1,8 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import {Comment} from '../../models/workday.model';
 import {MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { User } from 'src/app/models/user.model';
+import { FirebaseService } from 'src/app/services/firebase.service';
 
 @Component({
   selector: 'app-comment-list',
@@ -10,11 +12,23 @@ import {MAT_DIALOG_DATA} from '@angular/material/dialog';
 export class CommentListComponent implements OnInit {
   comments: Comment[] = [];
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any) {
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any, private firebaseService: FirebaseService) {
     this.comments = data.comments;
   }
 
   ngOnInit() {
+    this.comments.forEach(async comment => {
+      await this.getImageUrl(comment.client);
+    });
+  }
+
+  async getImageUrl(user: User) {
+    return new Promise( (resolve, reject) => {
+      this.firebaseService.lookupFileDownloadUrl(user.picture, 'user').toPromise()
+      .then(image => resolve(user.picture = image))
+      .catch((e) => reject(e));
+    })
+    .catch((err) => console.log(err));
   }
 
   mailto(comment: Comment): string {
