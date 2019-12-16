074100e09ec2457c6b3b317533805ca7450e253b
Jakob Lierman <jakob.lierman@gmail.com>
2019-12-16 16:06:37 +0100

    Cleanup and popups

--- src/app/schedule/comment-new/comment-new.component.ts
+++ src/app/schedule/comment-new/comment-new.component.ts
@@ -1,11 +1,12 @@
-import {Component, OnInit, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup} from '@angular/forms';
-import * as $ from 'jquery';
 import {AuthenticationService} from '../../authentication/authentication.service';
 import {WorkdayDataService} from '../../services/workday.data.service';
-import {Workday, Comment} from '../../models/workday.model';
+import {Comment, Workday} from '../../models/workday.model';
 import {Router} from '@angular/router';
-import {HttpErrorResponse} from '@angular/common/http';
+import {SuccessModalComponent} from '../../shared/success-modal/success-modal.component';
+import {MatDialog} from '@angular/material/dialog';
+import {ErrorModalComponent} from '../../shared/error-modal/error-modal.component';
 
 @Component({
   selector: 'app-comment-new',
@@ -14,24 +15,25 @@ import {HttpErrorResponse} from '@angular/common/http';
 })
 export class CommentNewComponent implements OnInit {
   @Input() private workday: Workday;
-  isAdmin = true;
+  isAdmin = false;
   commentObject: Comment;
   userComment: string;
   public commentForm: FormGroup;
-  public errorMsg = '';
 
   constructor(
-      private auth: AuthenticationService,
-      private workdayDataService: WorkdayDataService,
-      private router: Router,
-      private fb: FormBuilder
-      ) {}
+    private auth: AuthenticationService,
+    public dialog: MatDialog,
+    private workdayDataService: WorkdayDataService,
+    private router: Router,
+    private fb: FormBuilder
+  ) {
+    this.isAdmin = this.auth.currentUser.admin;
+  }
 
   ngOnInit() {
     this.commentForm = this.fb.group({
       comment: {value: [''], disabled: this.isAdmin}
     });
-    this.isAdmin = this.auth.currentUser.admin;
     this.commentObject = this.getUserComment();
   }
 
@@ -55,52 +57,35 @@ export class CommentNewComponent implements OnInit {
       this.workdayDataService.patchComment(this.workday, this.commentObject).subscribe(
         val => {
           if (val) {
-            if (this.auth.redirectUrl) {
-              this.router.navigateByUrl(this.auth.redirectUrl);
-              this.auth.redirectUrl = undefined;
-            } else {
-              // TODO - weergeven dat het gelukt is
-              console.log(this.commentObject.comment);
-              console.log('Opmerking aangepast');
-            }
+            // TODO - weergeven dat het gelukt is
+            console.log(this.commentObject.comment);
+            this.dialog.open(SuccessModalComponent, {
+              width: '300px',
+              data: {message: 'Opmerking aangepast'}
+            });
           } else {
-            this.errorMsg = `Opmerking aanpassen mislukt`;
+            this.dialog.open(ErrorModalComponent, {
+              width: '300px',
+              data: {message: 'Opmerking aanpassen mislukt'}
+            });
           }
-        },
-        (err: HttpErrorResponse) => {
-          console.log(err);
-          if (err.error instanceof Error) {
-            this.errorMsg = `${err.error.message}`;
-          } else {
-            this.errorMsg = `${err.error}`;
-          }
-          $('#errorMsg').slideDown(200);
         }
       );
     } else {
       this.workdayDataService.postComment(this.workday, new Comment(this.commentForm.value.comment, this.auth.currentUser)).subscribe(
         val => {
           if (val) {
-            if (this.auth.redirectUrl) {
-              this.router.navigateByUrl(this.auth.redirectUrl);
-              this.auth.redirectUrl = undefined;
-            } else {
-              // TODO - updaten van commentobject, als je nu een comment aanmaakt en direct daarna aanpast, crasht hij
-              // TODO - weergeven dat het gelukt is
-              console.log('Opmerking toegevoegd');
-            }
+            // TODO - updaten van commentobject, als je nu een comment aanmaakt en direct daarna aanpast, crasht hij
+            this.dialog.open(SuccessModalComponent, {
+              width: '300px',
+              data: {message: 'Opmerking toegevoegd'}
+            });
           } else {
-            this.errorMsg = `Opmerking toevoegen mislukt`;
+            this.dialog.open(ErrorModalComponent, {
+              width: '300px',
+              data: {message: 'Opmerking toevoegen mislukt'}
+            });
           }
-        },
-        (err: HttpErrorResponse) => {
-          console.log(err);
-          if (err.error instanceof Error) {
-            this.errorMsg = `${err.error.message}`;
-          } else {
-            this.errorMsg = `${err.error}`;
-          }
-          $('#errorMsg').slideDown(200);
         }
       );
     }
