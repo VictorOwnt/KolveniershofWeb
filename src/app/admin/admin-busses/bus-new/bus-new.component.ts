fc92078f6f073adb25a36907b20ed6bf005f4a08
Jakob Lierman <jakob.lierman@gmail.com>
2019-12-18 01:35:15 +0100

    New bus cleanup

--- src/app/admin/admin-busses/bus-new/bus-new.component.ts
+++ src/app/admin/admin-busses/bus-new/bus-new.component.ts
@@ -1,12 +1,12 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
+import {MAT_DIALOG_DATA, MatDialog, MatDialogRef} from '@angular/material/dialog';
 import {AuthenticationService} from '../../../authentication/authentication.service';
 import {Router} from '@angular/router';
 import {HttpErrorResponse} from '@angular/common/http';
-import * as $ from 'jquery';
 import {Bus} from '../../../models/bus.model';
 import {BusDataService} from '../../../services/bus.data.service';
+import {ErrorModalComponent} from '../../../shared/error-modal/error-modal.component';
 
 
 @Component({
@@ -15,15 +15,14 @@ import {BusDataService} from '../../../services/bus.data.service';
   styleUrls: ['./bus-new.component.scss']
 })
 export class BusNewComponent implements OnInit {
-
   bus: Bus;
   public color = '#000000';
   public busForm: FormGroup;
-  public errorMsg = '';
 
   constructor(
     @Inject(MAT_DIALOG_DATA) private data: any,
     public dialogRef: MatDialogRef<BusNewComponent>,
+    private dialog: MatDialog,
     private authService: AuthenticationService,
     private router: Router,
     private fb: FormBuilder,
@@ -42,7 +41,6 @@ export class BusNewComponent implements OnInit {
     });
   }
 
-
   getNameErrorMessage() {
     return (this.busForm.controls.name.hasError('required'))
       ? 'Naam is verplicht.' : '';
@@ -52,28 +50,22 @@ export class BusNewComponent implements OnInit {
     if (this.bus) {
       this.bus.name = this.busForm.value.name;
       this.bus.color = this.color;
-      console.log(this.bus);
       this.busDataService.patchBus(this.bus).subscribe(
         val => {
           if (val) {
-            if (this.authService.redirectUrl) {
-              this.router.navigateByUrl(this.authService.redirectUrl);
-              this.authService.redirectUrl = undefined;
-            } else {
-              this.dialogRef.close();
-            }
+            // Success modal
+            this.dialogRef.close('Bus aangepast.');
           } else {
-            this.errorMsg = `Bus aanpassen mislukt`;
+            // Error modal
+            this.dialogRef.close(false);
           }
         },
         (err: HttpErrorResponse) => {
-          console.log(err);
-          if (err.error instanceof Error) {
-            this.errorMsg = `${err.error.message}`;
-          } else {
-            this.errorMsg = `${err.error}`;
-          }
-          $('#errorMsg').slideDown(200);
+          // Open error dialog
+          this.dialog.open(ErrorModalComponent, {
+            width: '300px',
+            data: {message: err.error instanceof Error ? err.error.message : err.error}
+          });
         }
       );
     } else {
@@ -81,24 +73,19 @@ export class BusNewComponent implements OnInit {
       this.busDataService.postBus(bus).subscribe(
         val => {
           if (val) {
-            if (this.authService.redirectUrl) {
-              this.router.navigateByUrl(this.authService.redirectUrl);
-              this.authService.redirectUrl = undefined;
-            } else {
-              this.dialogRef.close();
-            }
+            // Success modal
+            this.dialogRef.close('Bus aangemaakt.');
           } else {
-            this.errorMsg = `Bus aanmaken mislukt`;
+            // Error modal
+            this.dialogRef.close(false);
           }
         },
         (err: HttpErrorResponse) => {
-          console.log(err);
-          if (err.error instanceof Error) {
-            this.errorMsg = `${err.error.message}`;
-          } else {
-            this.errorMsg = `${err.error}`;
-          }
-          $('#errorMsg').slideDown(200);
+          // Open error dialog
+          this.dialog.open(ErrorModalComponent, {
+            width: '300px',
+            data: {message: err.error instanceof Error ? err.error.message : err.error}
+          });
         }
       );
     }
